1ea49ae2737b6e6e78afdd625f2543c376a8678a
Наталья Вырлан <vyrlan.natali@mail.ru>
2020-06-19 13:06:15 +0300

    add shop

--- MyProject/Views/Models/Products.cs
+++ MyProject/Views/Models/Products.cs
@@ -16,7 +16,7 @@ namespace MyProject.Models
 
         public string Name { get; set; }
 
-        public string Description { get; set; }
+        ///public string Description { get; set; }
 
         public decimal Price { get; set; }
         public byte[] Image { get; set; }
